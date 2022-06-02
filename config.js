f2af6f8b9a1e06738648f0fe568c1186ee2dac1d
MacroKeke <106157697+MacroKeke@users.noreply.github.com>
2022-06-02 18:22:07 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// Repo Sahibi - github.com/sirincay
+// Telegram - t.me/HusnuEhedov
+////////////////////////////////////////
+// Başka github hesabına yükləməy olmaz
+// Reponu öz adına çıxaran peysərdi...!!!
+
+module.exports = ({
+
+token: "5567421605:AAFd5FRbI6vc4MvCMdnUIcfRcyPOcBsMRPM",
+startmesaj: " <b><i>Selam. Bota'ya itiraf etmek istediğiniz mesajı yazıyorsunuz ve kanalda paylaşılıyor..\n\nİtirafınızı yazdıktan sonra anonim veya açık olarak paylaşmanızı rica edeceğim. ✅\n\nBuyur Bir itiraf Et 💁🏼‍♀️</i></b>",
+aciqetirafeden: "📣 Açık Etiraf \n👥itirafçı: ",
+Açıkça itiraf etti: "💄Yazılı İtiraf: ",
+sonluq: "⭐️ itiraf etmek için: @MajesteitirafXBot",
+anonimetiraf: "✅ Paylaşım: Anonim itiraf",
+anonimyazdigietiraf: "🔹 Yazdığı itiraf: ",
+yenidenetiraf: "Yeni itiraf Yazın 🔄",
+	
+});
